4f91a707d92299c30e1b3b9668b99865dce6da98
egork <egork@DESKTOP-5O38A2L>
2022-05-23 21:23:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,59 @@
+﻿using Person.Work;
+using Person;
+using ConsoleApp4;
+
+Person.person mixalych = new person("Name", "Surename", 42);
+mixalych.Print();
+work GazProm = new work("GazProm","Head",200000);
+GazProm.Print_2();
+namespace Person
+{
+    class person
+    {
+        public string name;
+        public string surename;
+        public int age;
+        public person(string name,string surename ,int age)
+        {
+            this.name = name;
+            this.surename = surename;
+            this.age = age;
+        }
+        public void Print()
+        {
+            Console.WriteLine(name+" "+surename+" "+age);
+        }
+        public void Destroer(out string name,out string surename,out int age)
+        {
+            name=this.name;
+            surename=this.surename;
+            age=this.age;
+        }
+    }
+    namespace Work
+    {
+        class work
+        {
+            public string company_name;
+            public string name_worker;
+            public double sallary;
+            public void Print_2()
+            {
+                Console.WriteLine(company_name + " " + name_worker + " " + sallary);
+            }
+            public work(string company_name ,string name_worcker, double sallary)
+            {
+                this.company_name = company_name;
+                this.name_worker=name_worcker;
+                this.sallary = sallary;
+            }
+            public void Destructor( out string company_name, out string name_worker,out double sallary)
+            {
+                company_name=this.company_name;
+                name_worker = this.name_worker;
+                sallary=this.sallary;
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp4/locale_live.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class locale_live
+    {
+        public string country;
+        public string region;
+        public string city;
+        public string street;
+        public int number_house;
+
+        public locale_live(string country,string region,string city,string street,int number_house)
+        {
+            this.country = country;
+            this.region = region;
+            this.city = city;
+            this.street = street;
+            this.number_house = number_house;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Country: {country} Region: {region} City: {city} Street: {street} Number house: {number_house}");
+        }
+        public void DeadClass(out string country, out string region, out string city, out string street, out int number_house)
+        {
+            country =this.country;
+            region =this.region;
+            city =this.city;
+            street =this.street;
+            number_house =this.number_house;
+        }
+
+    }
+}
